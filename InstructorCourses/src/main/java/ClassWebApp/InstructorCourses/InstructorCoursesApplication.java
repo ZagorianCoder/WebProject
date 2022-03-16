5bb8a58218a2a099fce608d3f0a9edbc0e8e929c
chris <chriszafiris13@gmail.com>
2022-03-16 20:57:42 +0200

    Whole project Initial files

---
+++ InstructorCourses/src/main/java/ClassWebApp/InstructorCourses/InstructorCoursesApplication.java
@@ -0,0 +1,13 @@
+package ClassWebApp.InstructorCourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstructorCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstructorCoursesApplication.class, args);
+	}
+
+}
